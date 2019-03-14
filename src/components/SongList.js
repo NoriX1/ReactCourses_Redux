dbb2293d1eefdaa216f3cb8a4370bedaf5b3c010
NoriX <versatility.95@mail.ru>
2019-03-14 19:56:41 +0300

    Using connect function to receive props for component

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -7,4 +7,10 @@ class SongList extends React.Component {
     }
 }
 
-export default connect()(SongList);
+const mapStateToProps = (state) => {
+    return { songs: state.songs };
+    // this.props = {songs : state.songs}
+};
+
+
+export default connect(mapStateToProps)(SongList);
