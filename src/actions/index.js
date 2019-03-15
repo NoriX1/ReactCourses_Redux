c0fc70373d015d570d0d1400058ba2e35c5ece53
NoriX <versatility.95@mail.ru>
2019-03-15 20:20:59 +0300

    Second way to solve issue with requests (using new action)

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,26 @@
 import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+    const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    userIds.forEach(id => dispatch(fetchUser(id)));
+};
+
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts');
 
     dispatch({ type: 'FETCH_POSTS', payload: response.data })
 };
 
-// Refactored from :
+export const fetchUser = (id) => async dispatch => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+
+    dispatch({ type: 'FETCH_USER', payload: response.data })
+
+};
+
+// fetchPosts Refactored from :
 // export const fetchPosts = () => {
 //     return async function (dispatch, getState) {
 //         const response = await jsonPlaceholder.get('/posts');
@@ -18,15 +31,12 @@ export const fetchPosts = () => async dispatch => {
 //     };
 // };
 
-export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+// ------------Using Memoize to solve issue with requests-----------------
+// But we have issue: if data being updated, function will return old data.
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
 
-// Refactored from :
-// export const fetchUser = (id) => dispatch => {
-//     _fetchUser(id, dispatch);
-// };
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
 
-const _fetchUser = _.memoize(async (id, dispatch) => {
-    const response = await jsonPlaceholder.get(`/users/${id}`);
-
-    dispatch({ type: 'FETCH_USER', payload: response.data })
-});
+//     dispatch({ type: 'FETCH_USER', payload: response.data })
+// });
